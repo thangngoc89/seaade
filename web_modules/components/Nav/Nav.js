e8da6f10f5992ffc8377e305056640b610681900
Khoa Nguyen <khoa02031995@gmail.com>
2016-03-03 01:36:20 +0700

    Use react-bootstrap for smooth bootstrap animation

--- web_modules/components/Nav/Nav.js
+++ web_modules/components/Nav/Nav.js
@@ -1,46 +1,25 @@
-import React, { Component } from "react"
+import React, { Component, PropTypes } from "react"
 import cx from "classnames"
 import { Link } from "statinamic/lib/Link"
+import Navbar from "react-bootstrap/lib/Navbar"
+import Nav from "react-bootstrap/lib/Nav"
+import NavDropdown from "react-bootstrap/lib/NavDropdown"
 
 import styles from "./Nav.scss"
 
-class Nav extends Component {
+export default class NavComponent extends Component {
+  static propTypes = {
+    docked: PropTypes.bool.isRequired,
+  };
+
   constructor(props) {
     super(props)
 
     this.state = {
-      docked: false,
       collaspe: true,
     }
 
-    this.attachNavScroll = this.attachNavScroll.bind(this)
     this.handleCollaspeToggle = this.handleCollaspeToggle.bind(this)
-    this.dockNav = this.dockNav.bind(this)
-  }
-
-  componentDidMount() {
-    this.attachNavScroll()
-  }
-
-  componentDidUpdate() {
-    this.attachNavScroll()
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.dockNav)
-  }
-  attachNavScroll() {
-    window.addEventListener("scroll", this.dockNav)
-  }
-
-  dockNav() {
-    const docked = document.body.scrollTop > 10
-
-    if (this.state.docked !== docked) {
-      this.setState({
-        docked,
-      })
-    }
   }
 
   handleCollaspeToggle() {
@@ -51,75 +30,52 @@ class Nav extends Component {
 
   render() {
     const navClass = cx(styles.wrapper, {
-      [styles.docked]: this.state.docked,
+      [styles.docked]: this.props.docked,
     })
-    const collaspeClass = cx("navbar-collapse", {
-      collapse: this.state.collaspe,
-    })
+
     return (
-      <nav id="site-nav" className={ navClass }>
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <div className="site-branding">
-              <Link
-                to="/"
-                className="logo"
-              >
-                SEAADE 2016
-              </Link>
-            </div>
-            <button
-              onClick={ this.handleCollaspeToggle }
-              className="navbar-toggle collapsed"
+      <Navbar fluid fixedTop className={ navClass }>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link
+              to="/"
+              className={styles.logo}
             >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-          </div>
-          <div className={ collaspeClass }>
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/" activeClassName="active">Home</Link>
-              </li>
-              <li>
-                <Link to="/important-dates/" activeClassName="active">Important Dates</Link>
-              </li>
-              <li className="dropdown">
-                <Link
-                  to="/events/"
-                  className="dropdown-toggle"
-                  activeClassName="active"
-                >
-                  Events <span className="caret"></span>
-                </Link>
-                <ul className="dropdown-menu">
-                  <li><Link to="/scientific-program/" activeClassName="active">Scientific Program</Link></li>
-                  <li><Link to="/events/" activeClassName="active">GC Prevention Table Clinic Competition</Link></li>
-                </ul>
-              </li>
-              <li>
-                <Link to="/abstract-submission/" activeClassName="active">Abstract Submission</Link>
-              </li>
-              <li>
-                <Link to="/registration/" activeClassName="active">Registration</Link>
-              </li>
-              <li>
-                <Link to="/general-info/" activeClassName="active">General Info</Link>
-              </li>
-              <li>
-                <Link to="/sponsors/" activeClassName="active">Sponsors</Link>
-              </li>
-              <li>
-                <Link to="/contact/" activeClassName="active">Contact</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+              SEAADE 2016
+            </Link>
+          </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <li>
+              <Link to="/" activeClassName="active">Home</Link>
+            </li>
+            <li>
+              <Link to="/important-dates/" activeClassName="active">Important Dates</Link>
+            </li>
+            <NavDropdown title="Events">
+              <li><Link to="/events/scientific-program/" activeClassName="active">Scientific Program</Link></li>
+              <li><Link to="/events/gc-prevention-table" activeClassName="active">GC Prevention Table Clinic Competition</Link></li>
+            </NavDropdown>
+            <li>
+              <Link to="/abstract-submission/" activeClassName="active">Abstract Submission</Link>
+            </li>
+            <li>
+              <Link to="/registration/" activeClassName="active">Registration</Link>
+            </li>
+            <li>
+              <Link to="/general-info/" activeClassName="active">General Info</Link>
+            </li>
+            <li>
+              <Link to="/sponsors/" activeClassName="active">Sponsors</Link>
+            </li>
+            <li>
+              <Link to="/contact/" activeClassName="active">Contact</Link>
+            </li>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       )
   }
 }
-
-export default Nav
--- web_modules/components/Nav/index.js
+++ web_modules/components/Nav/index.js
@@ -1,1 +1,45 @@
-export default from "./Nav"
+import React, { Component } from "react"
+import Nav from "./Nav"
+
+export default class NavDockable extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      docked: false,
+    }
+
+    this.dockNav = this.dockNav.bind(this)
+  }
+
+  componentDidMount() {
+    this.attachNavScroll()
+  }
+
+  componentDidUpdate() {
+    this.attachNavScroll()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.dockNav)
+  }
+  attachNavScroll() {
+    window.addEventListener("scroll", this.dockNav)
+  }
+
+  dockNav() {
+    const docked = document.body.scrollTop > 10
+
+    if (this.state.docked !== docked) {
+      this.setState({
+        docked,
+      })
+    }
+  }
+
+  render () {
+    return (
+      <Nav docked={this.state.docked} />
+    )
+  }
+}
