9ea0c1c89509dc697221ce2f514490bfee0a58ab
Đạt <lety_20391@yahoo.com>
2016-03-09 16:42:47 +0700

    add DMC logo

--- web_modules/layouts/Homepage/components/Sponsors/Sponsors.js
+++ web_modules/layouts/Homepage/components/Sponsors/Sponsors.js
@@ -2,6 +2,7 @@ import React from "react"
 import logoDent from "./logo-dentsply.png"
 import logoPs from "./logo-ps.png"
 import styles from "./Sponsors.scss"
+import logoDMC from "./logo-DMC.png"
 
 class Sponsors extends React.Component {
   render() {
@@ -14,18 +15,26 @@ class Sponsors extends React.Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-sm-6">
+            <div className="col-sm-5">
               <img
                 className={ styles.img }
                 src={ logoPs }
               />
             </div>
-            <div className="col-sm-6">
+            <div className="col-sm-7">
               <img
                 className={ styles.img }
                 src={ logoDent }
               />
             </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12">
+            <img className={styles.img}
+            src={logoDMC}
+            />
+            </div>
+
           </div>
         </div>
       </section>
