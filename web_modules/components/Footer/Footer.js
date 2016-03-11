db941f40809e5a8b73bdd0bb570e2c2d85dc5953
Đạt <lety_20391@yahoo.com>
2016-03-11 16:00:20 +0700

    Responsive footer

--- web_modules/components/Footer/Footer.js
+++ web_modules/components/Footer/Footer.js
@@ -7,12 +7,12 @@ class Footer extends React.Component {
       <footer className={ styles.footer }>
         <div className="container">
           <div className="row">
-            <div className="col-xs-6">
+            <div className="col-xs-12 col-md-6">
               <p className={ styles.info }>
                 Copyright © 2016, <a href="http://khoanguyen.me">Khoa Nguyen</a>
               </p>
             </div>
-            <div className="col-xs-6">
+            <div className="col-xs-12 col-md-6">
               <p className={ styles.fb }>
                 Faculty of Odonto-Stomatology, UMP<br/>
                 <a href="https://www.facebook.com/Seaade2016.VN/">Follow us on Facebook</a>
