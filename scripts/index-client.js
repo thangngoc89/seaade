247af0be5035ee1452d34dc3632482082ec6156f
Khoa Nguyen <hi@khoanguyen.me>
2016-04-07 20:11:44 +0700

    fix(compat): Add String.prototype.endsWith polyfill for Safari 8

--- scripts/index-client.js
+++ scripts/index-client.js
@@ -35,3 +35,20 @@ window.addEventListener("load", () => {
     }
   }, false)
 }, false)
+
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+    const subjectString = this.toString()
+    if (
+      typeof position !== "number" ||
+      !isFinite(position) ||
+      Math.floor(position) !== position ||
+      position > subjectString.length
+    ) {
+      position = subjectString.length
+    }
+    position -= searchString.length
+    const lastIndex = subjectString.indexOf(searchString, position)
+    return lastIndex !== -1 && lastIndex === position
+  }
+}
