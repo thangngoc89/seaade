7a46b3a9d4d3740e7a0f7fb6d9cc3f2032d7a4dc
Đạt <lety_20391@yahoo.com>
2016-05-25 16:32:50 +0700

    lalala

--- web_modules/components/Notification/index.js
+++ web_modules/components/Notification/index.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from "react"
 import styles from "./index.scss"
+import notifications from "../../notification.json"
 
 export default class Notification extends Component {
   static propTypes = {
@@ -11,7 +12,11 @@ export default class Notification extends Component {
     return (
       <div className={ styles.headWrap }>
         <div className="container">
-          <p>The SEAADE 2016 abstracts submission deadline extended until May 30<sup>th</sup>, 2016</p>
+          {
+            notifications.map((noti) => (
+              <p dangerouslySetInnerHTML={ { __html: noti.content } } />
+            ))
+          }
         </div>
       </div>
     )
